b9e2432d954f0a5eedb66559ab2bb79b139c93f8
lisen87 <453354858@qq.com>
2018-11-23 14:18:47 +0800

    Add files via upload

---
+++ KeepService.java
@@ -0,0 +1,62 @@
+package com.qiaotongtianxia.sevices;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.Service;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.IBinder;
+
+/**
+ * Created by lisen on 2018-10-27.
+ *
+ * @author lisen < 453354858@qq.com >
+ */
+
+public class KeepService extends Service {
+    public static final int NOTIFICATION_ID=0x11;
+
+    public KeepService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        //API 18以上，发送Notification并将其置为前台后，启动InnerService
+        Notification.Builder builder = new Notification.Builder(this);
+        builder.setSmallIcon(R.mipmap.ic_launcher);
+        startForeground(NOTIFICATION_ID, builder.build());
+        startService(new Intent(this, InnerService.class));
+    }
+
+    public  static class  InnerService extends Service{
+
+        @Override
+        public IBinder onBind(Intent intent) {
+            return null;
+        }
+        @Override
+        public void onCreate() {
+            super.onCreate();
+            //发送与KeepLiveService中ID相同的Notification，然后将其取消并取消自己的前台显示
+            Notification.Builder builder = new Notification.Builder(this);
+            builder.setSmallIcon(R.mipmap.ic_launcher);
+            startForeground(NOTIFICATION_ID, builder.build());
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    stopForeground(true);
+                    NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+                    manager.cancel(NOTIFICATION_ID);
+                    stopSelf();
+                }
+            },100);
+
+        }
+    }
+}
